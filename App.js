1c76762483159170780b5879316a6b2494326196
kkamara <kamaracomputers@gmail.com>
2024-01-21 23:24:27 +0000

    Add project

--- App.js
+++ App.js
@@ -1,7 +1,24 @@
 import { StatusBar } from 'expo-status-bar';
+import { useEffect, } from 'react'
 import { StyleSheet, Text, View } from 'react-native';
+import AsyncStorage from '@react-native-async-storage/async-storage'
 
 export default function App() {
+  useEffect(() => {
+    const getData = async () => {
+      try {
+        const value = await AsyncStorage.getItem('auth_key');
+        console.log(value)
+        if (value !== null) {
+          // value previously stored
+        }
+      } catch (e) {
+        // error reading value
+      }
+    }
+    getData()
+  })
+
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
